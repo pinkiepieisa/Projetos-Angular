4356cbef038058fce27b11d7cae5ba20d4dfb98a
Bela <imcdsls10@gmail.com>
2026-02-09 15:17:46 -0300

    Configuração HTML e CSS

---
+++ projeto_filtro/src/app/components/components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "../angular-material/angular-materia.module";
+
+
+@NgModule(
+    {
+        declarations: [],
+        imports: [
+            AngularMaterialModule,
+        ],
+        exports: []
+    }
+)
+export class ComponentsModule {
+
+}
