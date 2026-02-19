f326d1a832b70e6a03a5a7cc02ecfbe95a92b57a
Bela <imcdsls10@gmail.com>
2026-02-19 12:29:43 -0300

    Pipe da data

--- projeto_filtro/src/app/pipes/pipes.module.ts
+++ projeto_filtro/src/app/pipes/pipes.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
 import { AdressPipe } from './adress.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
     declarations: [
         PhonePipe,
         AdressPipe,
+        StatusPipe,
   ],
     exports: [
         PhonePipe,
         AdressPipe,
+        StatusPipe
     ],
 })
 
