a5cc04992f1925449e98043aa765d7d9b4178897
Bela <imcdsls10@gmail.com>
2026-02-19 12:07:32 -0300

    Pipe do endereço

--- projeto_filtro/src/app/pipes/pipes.module.ts
+++ projeto_filtro/src/app/pipes/pipes.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
+import { AdressPipe } from './adress.pipe';
 
 @NgModule({
     declarations: [
         PhonePipe,
+        AdressPipe,
   ],
     exports: [
         PhonePipe,
+        AdressPipe,
     ],
 })
 
