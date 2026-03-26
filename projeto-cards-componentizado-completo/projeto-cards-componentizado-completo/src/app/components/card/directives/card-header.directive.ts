2b4887b10cc61cab8b4a3a49a85b8afb4509a9db
Bela <imcdsls10@gmail.com>
2026-03-26 11:57:12 -0300

    Alteração da cor do card do cliente

--- projeto-cards-componentizado-completo/projeto-cards-componentizado-completo/src/app/components/card/directives/card-header.directive.ts
+++ projeto-cards-componentizado-completo/projeto-cards-componentizado-completo/src/app/components/card/directives/card-header.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: 'app-card-header',
   host: { 'class': 'ca-c-card__header' }
   // Interliga a diretiva ao scss global, atribuindo ele à ela
 })
-export class CardHeaderDirective {}
+
+export class CardHeaderDirective {
+  @Input()
+  @HostBinding('class') color: string = '';
+  //Para utilizar a classe a fim de alterar a cor do header
+}
