b190bf112c5404f3c977cae0138081c31036b19f
Bela <imcdsls10@gmail.com>
2026-05-04 16:58:31 -0300

    Genres Service

--- projeto-template-driven-forms/src/app/app.component.ts
+++ projeto-template-driven-forms/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
+import { GenresService } from './services/genres.service';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,28 @@ import { UsersService } from './services/users.service';
 })
 export class AppComponent implements OnInit {
   usersList: any = [];
+  genresList: any = [];
+
   constructor(
-    private readonly _usersService: UsersService
+    private readonly _usersService: UsersService,
+    private readonly _genresService: GenresService
   ) {}
 
   ngOnInit(){
     this.getUsers();
+    this.getGenres();
   }
 
   getUsers() {
     this._usersService.getUsers().subscribe((usersListResponse) => {
       this.usersList = usersListResponse;
     });
+  }
+
+  getGenres() {
+    this._genresService.getGenres().subscribe((genresListResponse) => {
+      this.genresList = genresListResponse;
+    })
+
   }
 }
