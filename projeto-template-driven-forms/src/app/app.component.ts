7547e05f4c368362a45e9e61743e6ba1c54c7a47
Bela <imcdsls10@gmail.com>
2026-05-05 13:00:57 -0300

    Criação dos Services

--- projeto-template-driven-forms/src/app/app.component.ts
+++ projeto-template-driven-forms/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
 import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,24 +11,33 @@ import { GenresService } from './services/genres.service';
 export class AppComponent implements OnInit {
   usersList: any = [];
   genresList: any = [];
+  statesList: any = [];
 
   constructor(
     private readonly _usersService: UsersService,
-    private readonly _genresService: GenresService
+    private readonly _genresService: GenresService,
+    private readonly _brazilianStatesService: BrazilianStatesService
   ) {}
 
   ngOnInit(){
     this.getUsers();
     this.getGenres();
+    this.getStates();
   }
 
-  getUsers() {
+  private getStates() {
+    this._brazilianStatesService.getStates().subscribe((statesListResponse) => {
+      this.statesList = statesListResponse;
+    });
+  }
+
+  private getUsers() {
     this._usersService.getUsers().subscribe((usersListResponse) => {
       this.usersList = usersListResponse;
     });
   }
 
-  getGenres() {
+  private getGenres() {
     this._genresService.getGenres().subscribe((genresListResponse) => {
       this.genresList = genresListResponse;
     })
--- projeto-template-driven-forms/src/app/services/genres.service.ts
+++ projeto-template-driven-forms/src/app/services/genres.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { observeNotification } from "rxjs/internal/Notification";
 
 @Injectable({
     providedIn: 'root'
