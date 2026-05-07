c49aae41908a85fbf350851ba6dc0c28bc66976a
Bela <imcdsls10@gmail.com>
2026-05-07 11:35:26 -0300

    Campo email e validador

--- projeto-template-driven-forms/src/app/directives/directives.module.ts
+++ projeto-template-driven-forms/src/app/directives/directives.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
+import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
 
 @NgModule({
-    declarations: [],
-    exports: [],
+    declarations: [
+      EmailPatternValidatorDirective
+    ],
+    exports: [
+      EmailPatternValidatorDirective
+    ],
 })
 
 export class DirectiveModule {
