adba8e8e223428da7bd1256a111347bcc94edcca
Bela <imcdsls10@gmail.com>
2026-06-09 12:18:34 -0300

    Service do UsersList

--- reactive-forms-project/src/app/app.component.ts
+++ reactive-forms-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 import { StatesService } from './services/states.service';
 import { CitiesService } from './services/cities.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit {
     private readonly _countriesService: CountriesService,
     private readonly _statesService: StatesService,
     private readonly _citiesService: CitiesService,
+    private readonly _usersService: UsersService,
   ) {}
   // Para realizar a injeção de dependência
 
@@ -31,6 +33,11 @@ export class AppComponent implements OnInit {
 
     this._citiesService.getCities('Brazil', 'São Paulo').subscribe((citiesResponse) => {
       console.log('citiesResponse', citiesResponse)
+    });
+    //GetCities
+
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      console.log('usersResponse', usersListResponse)
     });
   }
   
