624d300dba176bdb22b5050559bc38757b96ed64
Bela <imcdsls10@gmail.com>
2026-06-30 17:03:52 -0300

    Controllers

--- reactive-forms-project/src/app/components/user-informations-container/user-form-controller.ts
+++ reactive-forms-project/src/app/components/user-informations-container/user-form-controller.ts
@@ -1,6 +1,9 @@
 import { inject } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { IUser } from "../../interfaces/user/user.interface";
+import { PhoneList } from "../../types/phone-list";
+import { AddressList } from "../../types/address-list";
+import { DependentList } from "../../types/dependents-list";
 
 export class UserFormController {
     //Lógica do formulário
@@ -20,8 +23,60 @@ export class UserFormController {
     }
     //Alias do método get do generalInformations
 
+    get phoneList(): FormArray {
+        return this.userForm.get('contactInformations.phoneList') as FormArray;
+    }
+
+    get addressList(): FormArray {
+        return this.userForm.get('contactInformations.addressList') as FormArray;
+    }
+
+    get dependentsList(): FormArray {
+        return this.userForm.get('contactInformations.dependentsList') as FormArray;
+    }
+
     fulfillUserForm(user: IUser) {
         this.fulfillGeneralInformations(user);
+
+        this.fulfillPhoneList(user.phoneList);
+
+        this.fulfillAddressList(user.addressList);
+
+        this.fulfillDependentsList(user.dependentsList);
+    }
+
+    private fulfillDependentsList(userDependentsList: DependentList) {
+        userDependentsList.forEach((dependent) => {
+            this.dependentsList.push(this._fb.group({
+                name: [dependent.name, Validators.required],
+                age: [dependent.age, Validators.required],
+                document: [dependent.document, Validators.required], 
+            }));
+        });
+    }
+
+    private fulfillAddressList(userAddressList: AddressList) {
+        userAddressList.forEach((address) => {
+            this.addressList.push(this._fb.group({
+                type: [address.type, Validators.required],
+                street: [address.street, Validators.required],
+                complement: [address.complement, Validators.required],
+                country: [address.country, Validators.required],
+                state: [address.state, Validators.required],
+                city: [address.city, Validators.required],
+            }));
+        });
+    }
+
+    private fulfillPhoneList(userPhoneList: PhoneList) {
+        userPhoneList.forEach((phone) => {
+            this.phoneList.push(this._fb.group({
+                type: [phone.type, Validators.required],
+                areaCode: [phone.areaCode, Validators.required],
+                internationalCode: [phone.internationalCode, Validators.required],
+                number: [phone.number, Validators.required],
+            }));
+        });
     }
 
     private fulfillGeneralInformations(user: IUser) {
